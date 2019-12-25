fef0892c7c09dd8b0780fae8f6fed072f20e9d8a
GuanR <625097445@qq.com>
2019-12-25 18:03:16 +0800

    addorm

---
+++ orm/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2c06ac4-3f80-4948-a1df-9a9d57494bac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ orm/UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ormHelper orm = new ormHelper();
+            HR_SMSInfo model = orm.Get<HR_SMSInfo>("f6c5b6dd-ba9c-4fc5-a07c-081ab1522d40");
+        }
+    }
+}
